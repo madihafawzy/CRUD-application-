ce899ac6e0bb37b15b8d1d955bf8a338b4148219
madihafawzy <madiha_fawzy76@yahoo.comgit config --global user.name Your>
2018-03-07 09:54:44 +0100

    modified

--- index.php
+++ index.php
@@ -9,6 +9,12 @@
 <html>
 
 <head>
+     <title></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+   
 
     <title>PHP CRUD</title>
 
